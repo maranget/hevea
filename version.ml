3683726dc4edda27174ff9e1704fd41cc49594cf
Luc Maranget <Luc.Maranget@inria.fr>
1999-12-13 16:01:17 +0000

    version

--- version.ml
+++ version.ml
@@ -9,9 +9,9 @@
 (*                                                                     *)
 (***********************************************************************)
 
-let header = "$Id: version.ml,v 1.20 1999-12-08 09:17:24 maranget Exp $" 
+let header = "$Id: version.ml,v 1.21 1999-12-13 16:01:17 maranget Exp $" 
 let real_version = "1.05-6"
-let release_date = "1999-12-08"
+let release_date = "1999-12-13"
 
 let version =
   try
