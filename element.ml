8b3445a1dca943a77234cc3348b5432b53171dca
Luc Maranget <Luc.Maranget@inria.fr>
2000-05-30 12:30:14 +0000

    oubli

---
+++ element.ml
@@ -0,0 +1,23 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                          HEVEA                                      *)
+(*                                                                     *)
+(*  Luc Maranget, projet PARA, INRIA Rocquencourt                      *)
+(*                                                                     *)
+(*  Copyright 1998 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+let header =  "$Id: element.ml,v 1.1 2000-05-30 12:30:14 maranget Exp $"
+(* For text-level elements *)
+type text =
+  Style of string
+| Font of int
+| Color of string
+
+let pretty_text = function
+  Style s -> "Style: "^s
+| Font i  -> "Font size: "^string_of_int i
+| Color s  -> "Font color: "^s
+;;
---
+++ element.mli
@@ -0,0 +1,17 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                          HEVEA                                      *)
+(*                                                                     *)
+(*  Luc Maranget, projet PARA, INRIA Rocquencourt                      *)
+(*                                                                     *)
+(*  Copyright 1998 Institut National de Recherche en Informatique et   *)
+(*  Automatique.  Distributed only by permission.                      *)
+(*                                                                     *)
+(***********************************************************************)
+
+type text =
+  Style of string
+| Font of int
+| Color of string
+
+val pretty_text : text -> string
