b47b50570876fb676ab4bf209bfcaa565a5ec90b
Luc Maranget <Luc.Maranget@inria.fr>
2006-04-12 13:01:19 +0000

    +

--- version.ml
+++ version.ml
@@ -9,8 +9,8 @@
 (*                                                                     *)
 (***********************************************************************)
 
-let header = "$Id: version.ml,v 1.135 2006-04-03 17:37:39 maranget Exp $" 
-let real_version = "1.08+11"
+let header = "$Id: version.ml,v 1.136 2006-04-12 13:01:19 maranget Exp $" 
+let real_version = "1.08+12"
 let release_date = "2006-04-03"
 
 let version =
