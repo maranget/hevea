2b864b09fd2c7d68c76de337770fba569d995160
Luc Maranget <Luc.Maranget@inria.fr>
2007-02-12 13:44:12 +0000

    tag

--- release/mktag.sh
+++ release/mktag.sh
@@ -11,14 +11,19 @@ esac
 echo CVSDIR=${HOME}/CVS
 echo VERSION=${VERSION}
 echo RELEASENAME=hevea-\${VERSION}
+
 case $VERSION in
   test)
     echo CVSEXPORT=\"-D now\"
     ;;
   *)
+    TMP=/tmp/tag.$$
     RELEASETAG=`sed -n -e 's/^let real_version = "\(.\)\.\(.*\)".*$/\1-\2/p' ${VERSIONFILE}`
     echo RELEASETAG=$RELEASETAG
     echo CVSEXPORT=\"-r \${RELEASETAG}\"
+    sed  -e "s/^let release_date = .*/let release_date = \"`date +%Y-%m-%d`\"/" ${VERSIONFILE} > $TMP && mv $TMP $VERSIONFILE
+    ( cd .. ; cvs commit -m tag )
+    ( cd .. ; tar -F $RELEASETAG )
     ;;
 esac
 
