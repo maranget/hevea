a56d0e5ce7f0e4b9c186bc3559d960df053690e7
Luc Maranget <Luc.Maranget@inria.fr>
2017-07-26 07:55:29 +0000

    ready for v2.30

--- version.ml
+++ version.ml
@@ -9,8 +9,8 @@
 (*                                                                     *)
 (***********************************************************************)
 
-let real_version = "2.29+5"
-let release_date = "2017-05-18"
+let real_version = "2.30"
+let release_date = "2017-07-26"
 
 
 let version =
