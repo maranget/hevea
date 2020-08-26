maranget/hevea

commit a3040de008f9a50c5e93fdaf44e156efa0795057

CSS class for \verb and visual spaces for starred versions (#34)
Associate `\verb` and `\verb*` (which translate into `code` elements)
with CSS class `verb`.

In `\verb*` and `verbatim` environments use the Unicode "visible
space" character to indicate spaces or tabs instead of an ASCII
underscore.

verb.mll
@@ -995,20 +995,20 @@ let _ = ()
 ;;
 
 let put_char_star c next = match c with
-  | ' '|'\t' -> Dest.put_char '_' ;
+  | ' ' | '\t' -> Dest.put_unicode OutUnicode.visible_space;
   | c -> Scan.translate_put_unicode c next
 
 and put_char c next = match c with
-  |  '\t' -> Dest.put_char ' '
-  | c ->  Scan.translate_put_unicode c next
+  | '\t' -> Dest.put_char ' '
+  | c -> Scan.translate_put_unicode c next
 ;;
 
 
 let open_verb put lexbuf =
-  Dest.open_group "code" ;
+  Dest.open_group "code class=\"verb\"";
   start_inverb put lexbuf
 ;;
-  
+
 def_code "\\verb" (open_verb put_char) ;
 def_code "\\verb*" (open_verb put_char_star);
 ();;

doc/text.tex
@@ -4657,21 +4657,21 @@ This yields:
 \item Second item.
 \end{list}
 
-
 The \verb+trivlist+ environment is also supported. It is equivalent to
 the \verb+description+ environment.
+
 
 \subsection{Verbatim}
 
 The \verb+verbatim+ and \verb+verbatim*+ environments translate to
-the \verb+PRE+ element.
-Inside \verb+verbatim*+, spaces are replaced by underscores (``\verb*+ +'').
-
-
-Similarly, \verb+\verb+ and \verb+\verb*+
-translate to the \verb+CODE+ text element.
+the \verb+PRE+~elements.  Inside \verb+verbatim*+, spaces are replaced
+by visual spaces (``\verb*+ +'').  Similarly, \verb+\verb+
+and \verb+\verb*+ translate to \verb+CODE+ text elements.  The
+environments are associated with CSS~classes of the same names, this
+is, \verb+verbatim+ and \verb+\verb+, respectively.
 
 The \verb+alltt+ environment is supported.
+
 
 \section{Mathematical Formulae}
 

outUnicode.ml
@@ -834,11 +834,12 @@ let html_put put put_char i = match i with
 
 (* Constants *)
 
-let space = 0X20
-and nbsp = 0XA0
+let space = 0x20
+and nbsp = 0xA0
+and visible_space = 0x2423
 and acute_alone = 0xB4
-and grave_alone = 0X60
-and circum_alone = 0X5E
+and grave_alone = 0x60
+and circum_alone = 0x5E
 and diaeresis_alone = 0xA8
 and cedilla_alone = 0xB8
 and tilde_alone = 0x7E

outUnicode.mli
@@ -67,6 +67,7 @@ val html_put : (string -> unit) -> (char -> unit) -> unichar -> unit
 val null : unichar
 val space : unichar
 val nbsp : unichar
+val visible_space : unichar
 val acute_alone : unichar
 val grave_alone : unichar
 val circum_alone : unichar

commit 4b4c8aa53597d711f2ca1342de8630b17923808d

Fix HTML-generation bug introduced in a3040de008f9a50c5e93fdaf44e156efa0795057.
Problem: `Dest.open_group "code class=\"verb\""' constructs the HTML group
    <code class="verb">
    ...
    </code class="verb">
which is invalid HTML as closing elements never have attributes.

Solution: Use Dest.open_block which allows to pass an attribute.

En passant: Associate `\verb' with CSS-class `verb' via
`\setenvclass'.

html/hevea.hva
@@ -704,8 +704,9 @@
 #1%
 \fi\fi
 \@close{span@inline@block}}
-%%%%%%%%Default env class for verbatim
+%%%%%%%%Default env classes for verbatim and verb
 \setenvclass{verbatim}{verbatim}
+\setenvclass{verb}{verb}
 %%%%%% format theorems
 \newcommand{\set@th@font}[1]{\def\th@font{#1}}
 \let\th@font\em

verb.mll
@@ -834,7 +834,7 @@ let command_name =
 rule inverb verb_delim put = parse
 |  (_ as c)
     {if c = verb_delim then begin
-      Dest.close_group () ;
+      Dest.close_block "code";
     end else begin
       put c (fun () -> read_lexbuf lexbuf) ;
       inverb verb_delim put lexbuf
@@ -1004,8 +1004,10 @@ and put_char c next = match c with
 ;;
 
 
+let getclass env = Scan.get_prim (Printf.sprintf "\\envclass@attr{%s}" env)
+
 let open_verb put lexbuf =
-  Dest.open_group "code class=\"verb\"";
+  Dest.open_block ~force_inline:true "code" (getclass "verb");
   start_inverb put lexbuf
 ;;
 
@@ -1030,8 +1032,6 @@ let noeof lexer lexbuf =
         (Misc.Close
            ("End of file in environment: ``"^ !Scan.cur_env^"'' ("^s^")"))
   | EndVerb -> ()
-
-let getclass env = Scan.get_prim (Printf.sprintf "\\envclass@attr{%s}" env)
 
 let open_verbenv star =
   Scan.top_open_block "pre" (getclass "verbatim") ;