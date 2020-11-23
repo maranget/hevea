16cb105481843a634b7571a851d8dc6d91a59722
Chris Spiel <cspiel@freenet.de>
2020-11-23 10:07:20 +0100

    Equip the programs Hevea, Hacha, and Esponja with usage messages. Add manual pages for these programs, too.

--- hacha.ml
+++ hacha.ml
@@ -9,51 +9,59 @@
 (*                                                                     *)
 (***********************************************************************)
 
-open Printf
 
 exception Error of string
-;;
+
+
+let default_outname = "index.html"
+and default_small_length = 1024
 
 let filename = ref None
-let outname = ref "index.html"
-let log = ref false
-let toc_style = ref Cut.Normal
-let svg_arrows = ref true
-let cross_links = ref true
-let verbose = ref 0
-let small_length = ref 1024
+and outname = ref default_outname
+and log = ref false
+and toc_style = ref Cut.Normal
+and svg_arrows = ref true
+and cross_links = ref true
+and verbose = ref 0
+and small_length = ref default_small_length
 
-  
+
 let main () =
-  let spec =
-     [("-o", Arg.String (fun s -> outname := s),
-       "filename, make hacha output go into file 'filename' (defaults to index.html)");
-     ("-tocbis", Arg.Unit (fun () -> toc_style := Cut.Both),
-       ", Duplicate table of contents at the begining of files");      
-     ("-tocter", Arg.Unit (fun () -> toc_style := Cut.Special),
-       ", Insert most of table of contents at the beginning of files");
-     ("-no-svg-arrows", Arg.Unit (fun () ->  svg_arrows := false ),
-       ", Use gif arrows for the previous/up/next links in generated pages");
-     ("-nolinks", Arg.Unit (fun () -> cross_links := false),
-       ", Suppress the prevous/up/next links in generated pages");
-     ("-hrf", Arg.Unit (fun () -> log := true),
-        ", output a log file showing the association from local anchors to files");
-  ("-rsz", Arg.Int (fun i -> small_length := i),
-   (sprintf
-     "size of leaves in rope implementation (default %i)"
-      !small_length));
-  ("-version", Arg.Unit
-     (fun () ->
-       print_endline ("hacha "^Version.version) ;
-       print_endline ("library directory: "^Mylib.static_libdir) ;
-       exit 0),
-   "show hacha version and library directory") ;
-     ("-v", Arg.Unit (fun () -> incr verbose),
-        ", verbose flag") ]
-
-  and usage = "Usage: hacha [options] htmlfile" in
-
-  Arg.parse spec (fun s -> filename := Some s) usage ;
+  let usage =
+    "Usage: hacha [OPTION...] HTML-FILE\n\
+     \n\
+     Split the Hevea-generated HTML-FILE into several HTML files at\n\
+     its logical boundaries (chapters or sections) while keeping all\n\
+     cross-references intact.  Propagate headers, footers, footnotes,\n\
+     and the like into the split HTML files.\n\
+     \n\
+     Options:"
+  and spec =
+    ["-o", Arg.Set_string outname,
+     "FILENAME redirect Hacha output into FILENAME (default: \"" ^ default_outname ^ "\")";
+     "-tocbis", Arg.Unit (fun () -> toc_style := Cut.Both),
+     " duplicate table of contents at the beginning of files";
+     "-tocter", Arg.Unit (fun () -> toc_style := Cut.Special),
+     " insert most of the table of contents at the beginning of files";
+     "-no-svg-arrows", Arg.Clear svg_arrows,
+     " use GIF arrows for the previous/up/next links in generated pages";
+     "-nolinks", Arg.Clear cross_links,
+     " suppress the previous/up/next links in generated pages";
+     "-hrf", Arg.Set log,
+     " write a log file that shows the association of local anchors to files";
+     "-rsz", Arg.Set_int small_length,
+     (Printf.sprintf "SIZE set SIZE (default: %i) of leaves in rope implementation" default_small_length);
+     "-v", Arg.Unit (fun () -> incr verbose),
+     " report progress";
+     "-version",
+     Arg.Unit
+       (fun () ->
+         print_endline ("hacha " ^ Version.version);
+         print_endline ("library directory: " ^ Mylib.static_libdir);
+         exit 0),
+     " output version information, library directory and exit"]
+  in
+  Arg.parse (Arg.align spec) (fun s -> filename := Some s) usage;
   let filename =
     match !filename with
     | None -> raise (Error "No argument given")
@@ -69,7 +77,7 @@ let main () =
     let name_out = !outname
     let toc_style = !toc_style
     let svg_arrows = !svg_arrows
-    let cross_links = !cross_links 
+    let cross_links = !cross_links
     let small_length = !small_length
   end in
 
@@ -77,9 +85,9 @@ let main () =
   let buf = Lexing.from_channel chan in
   Location.set filename buf ;
   C.start_phase () ;
-  ignore (C.do_lex buf) ;  
+  ignore (C.do_lex buf) ;
   close_in chan ;
-  Location.restore () ;  
+  Location.restore () ;
   let chan = try open_in filename with Sys_error s -> raise (Error ("File error: "^s)) in
   let buf = Lexing.from_channel chan in
   Location.set filename buf ;
@@ -93,8 +101,8 @@ let main () =
     Mysys.copy_from_lib_to_dir Mylib.libdir C.dir "contents_motif.svg"
   end
   else if some_links then begin
-    Mysys.copy_from_lib_to_dir Mylib.libdir C.dir "previous_motif.gif" ;  
-    Mysys.copy_from_lib_to_dir Mylib.libdir C.dir "next_motif.gif" ;  
+    Mysys.copy_from_lib_to_dir Mylib.libdir C.dir "previous_motif.gif" ;
+    Mysys.copy_from_lib_to_dir Mylib.libdir C.dir "next_motif.gif" ;
     Mysys.copy_from_lib_to_dir Mylib.libdir C.dir "contents_motif.gif"
   end
 ;;
--- parse_opts.ml
+++ parse_opts.ml
@@ -9,30 +9,28 @@
 (*                                                                     *)
 (***********************************************************************)
 
-open Printf
-open Misc
 
 type input = File of string | Prog of string
 
 let files = ref []
-;;
+
 
 let add_input s = files := File s :: !files
 and add_program s = files := Prog s :: !files
-;;
+
 
 (* use this to create your warnings if you wish to *)
 let frenchwarning = ref false
-;;
+
 
 type destination = Html | Text | Info
-;;
-let mathml = ref false
-;;
 
-(*to activate advanced entities*) 
+let mathml = ref false
+
+
+(*to activate advanced entities*)
 let moreentities = ref false
-;;
+
 
 (* NO NEED AFTER BABEL SUPPORT *)
 (*let language = ref English*)
@@ -44,160 +42,183 @@ and destination = ref Html
 and fixpoint = ref false
 and optimize = ref false
 
+let default_width = 72
+and default_small_length = 1024
 
-let width = ref 72
-let except = ref []
-let path = ref []
-let outname = ref ""
-let small_length = ref 1024
+let width = ref default_width
+and except = ref []
+and path = ref []
+and outname = ref ""
+and small_length = ref default_small_length
 
-let check_displayverb n = if n > 1 then displayverb := true
 
-let _ = Arg.parse
-    [
-  ("-version", Arg.Unit
-     (fun () ->
-       print_endline ("hevea "^Version.version) ;
-       print_endline ("library directory: "^Mylib.static_libdir) ;
-       exit 0),
-   "show hevea version and library directory") ;
-  ("-v",
-   Arg.Unit
-     (fun () -> readverb := !readverb + 1 ; check_displayverb !readverb),
-   "verbose flag, can be repeated to increase verbosity") ;
-  ("-dv", Arg.Unit (fun () -> displayverb := true),
-   "add borders to some block-level elements, so as to show hevea output structure") ;
-  ("-s", Arg.Unit (fun () -> silent := true),
-   "suppress warnings") ;
-  ("-I", Arg.String (fun s -> path := s :: !path),
-   "dir, add directory 'dir' to search path") ;
-  ("-e", Arg.String (fun s -> except := s :: !except),
-   "filename, prevent file 'filename' from being read") ;
-  ("-fix", Arg.Unit (fun () -> fixpoint := true),
-   "iterate Hevea until fixpoint") ;
-  ("-O", Arg.Unit (fun () -> optimize := true),
-   "call esponja to optimize HTML output") ;
-  ("-exec", Arg.String add_program,
-   "prog , execute external program 'prog', then read its result") ;
-  ("-francais",Arg.Unit (fun () -> frenchwarning := true),
-   "French mode (deprecated)") ;
-  ("-moreentities", Arg.Unit (fun () -> moreentities := true),
-   "Enable the output of some rare entities.") ;
-  ("-entities", Arg.Unit (fun () -> symbol_mode := Entity),
-   "Render symbols by using entities, this is the default") ;
-  ("-textsymbols", Arg.Unit (fun () -> symbol_mode := SText),
-   "Render symbols by english text") ;
+let check_displayverb n =
+  if n > 1 then
+    Misc.displayverb := true
 
-  ("-noiso",Arg.Unit (fun () -> Misc.warning "-noiso is deprecated, by default hevea output is ascii"),
-   "deprecated, does nothing") ;
-  ("-pedantic",Arg.Unit (fun () -> pedantic := true),
-   "be pedantic in interpreting HTML 4.0 transitional definition") ;
-  ("-mathml",Arg.Unit (fun() -> mathml := true),
-   "produces MathML output for equations, very experimental");
 
-  ("-text",Arg.Unit (fun () -> symbol_mode :=  SText ; destination := Text),
-   "output plain text");
-  ("-info",Arg.Unit (fun () -> symbol_mode :=  SText ; destination := Info),
-   "output info file(s)");
-  ("-w", Arg.String (fun s -> width := int_of_string s),
-   "width, set the output width for text or info output");
-  ("-rsz", Arg.Int (fun i -> small_length := i),
-   (sprintf
-     "size of leaves in rope implementation (default %i)"
-      !small_length));
-  ("-o", Arg.String (fun s -> outname := s),
-   "filename, make hevea output go into file 'filename'")
-]
-    (add_input)
-    ("hevea "^Version.version)
-;;
+let () =
+  let usage =
+    "Usage: hevea [OPTION...] [HVA-FILE...|LATEX-FILE...]\n\
+     \n\
+     Consult all HVA-FILEs and LATEX-FILEs; designate last LATEX-FILE\n\
+     as main-input file and translate it to HTML.  If no file is given\n\
+     or the last file is not a LATEX-FILE work in filter mode: read\n\
+     LaTeX-source from standard input and write to standard output.\n\
+     \n\
+     Options `-info' or `-text' switch the output format to GNU Info\n\
+     or plain text.\n\
+     \n\
+     Options:"
+  and spec =
+    ["-I", Arg.String (fun s -> path := s :: !path),
+     "DIR prepend directory DIR to search path";
+     "-e", Arg.String (fun s -> except := s :: !except),
+     "FILE exclude FILE from being processed";
+     "-fix", Arg.Set fixpoint,
+     " iterate Hevea until reaching a fixpoint";
+     "-info", Arg.Unit (fun () -> symbol_mode := SText; destination := Info),
+     " output GNU Info file(s)";
+     "-text", Arg.Unit (fun () -> symbol_mode := SText; destination := Text),
+     " output plain text";
+     "-o", Arg.Set_string outname,
+     "FILE redirect Hevea output to FILE";
+     "-s", Arg.Set Misc.silent,
+     " suppress warnings";
+     "-w", Arg.String (fun s -> width := int_of_string s),
+     (Printf.sprintf "WIDTH set the output WIDTH (default: %i) for text or Info output" default_width);
+     "-O", Arg.Set optimize,
+     " call Esponja to optimize the HTML output of Hevea";
+     "-exec", Arg.String add_program,
+     "PROG execute external program PROG, then read its result";
+     "-entities", Arg.Unit (fun () -> symbol_mode := Entity),
+     " render symbols by using entities (default)";
+     "-francais", Arg.Set frenchwarning,
+     " French mode (deprecated)";
+     "-noiso", Arg.Unit (fun () -> Misc.warning "-noiso is deprecated, by default hevea output is ASCII"),
+     " deprecated, does nothing";
+     "-mathml", Arg.Set mathml,
+     " generate MathML output for equations; VERY EXPERIMENTAL";
+     "-moreentities", Arg.Set moreentities,
+     " enable the output of some rare entities";
+     "-pedantic", Arg.Set pedantic,
+     " be pedantic in interpreting HTML 4.0 transitional";
+     "-textsymbols", Arg.Unit (fun () -> symbol_mode := SText),
+     " render symbols by English text";
+     "-dv", Arg.Set Misc.displayverb,
+     " add borders to some block-level elements";
+     "-rsz", Arg.Set_int small_length,
+     (Printf.sprintf "SIZE set SIZE (default: %i) of leaves in rope implementation" default_small_length);
+     "-v", Arg.Unit (fun () -> incr Misc.readverb; check_displayverb !Misc.readverb),
+     " verbose flag; repeat to increase verbosity";
+     "-version",
+     Arg.Unit
+       (fun () ->
+         print_endline ("hevea " ^ Version.version);
+         print_endline ("library directory: " ^ Mylib.static_libdir);
+         exit 0),
+     " output version information, library directory and exit"]
+  in
+    Arg.parse (Arg.align spec) add_input usage
+
 
 let warning s =
-  if not !silent || !verbose > 0 then begin
-    Location.print_pos () ;
-    prerr_string "Warning: " ;
-    prerr_endline s
-  end
-;;
+  if not !Misc.silent || !Misc.verbose > 0 then
+    begin
+      Location.print_pos ();
+      prerr_string "Warning: ";
+      prerr_endline s
+    end
+
 
 (* For correcting strange user (-exec prog en dernier) *)
 let rec ffirst = function
-  | [] -> None,[]
-  | Prog _ as arg::rem ->
-      let file, rest = ffirst rem in
-      file, arg::rest
-  | File _ as arg::rem ->
-      Some arg,rem
+  | [] -> None, []
+  | Prog _ as arg :: rem ->
+     let file, rest = ffirst rem in
+       file, arg :: rest
+  | File _ as arg :: rem ->
+     Some arg, rem
 ;;
 
 files :=
    match ffirst !files with
-   | None,rem -> rem
-   | Some arg,rem -> arg::rem
+   | None, rem -> rem
+   | Some arg, rem -> arg :: rem
 
 
-      
-let base_in,name_in,styles = match !files with
-| File x :: rest ->
-    if Filename.check_suffix x ".hva" then
-      "","", !files
-    else
-      let base_file = Filename.basename x in
-      begin try
-        let base =
-          if Filename.check_suffix base_file ".tex" then
-            Filename.chop_extension base_file
-          else
-            base_file in
-        base,x,rest
-      with Invalid_argument _ -> base_file, x,rest
-      end
-| []|Prog _::_ -> "","",!files
+
+let base_in, name_in, styles =
+  match !files with
+  | File x :: rest ->
+     if Filename.check_suffix x ".hva" then
+       "", "", !files
+     else
+       let base_file = Filename.basename x in
+         begin
+           try
+             let base =
+               if Filename.check_suffix base_file ".tex" then
+                 Filename.chop_extension base_file
+               else
+                 base_file in
+               base,x,rest
+           with Invalid_argument _ -> base_file, x, rest
+         end
+  | [] | Prog _ :: _ -> "", "", !files
+
 
 let filter = match base_in with "" -> true | _ ->  false
 ;;
 
-if filter then begin
-  if !fixpoint then
-    Misc.warning ("No fixpoint in filter mode");
-  fixpoint := false
-end
-;;
 
-let base_out = match !outname with
-| "" -> begin match base_in with
-  | "" -> ""
-  | _  -> Filename.basename base_in
-end      
-| name ->
-    let suff = match !destination with
-    | Html -> ".html"
-    | Text -> ".txt"
-    | Info -> ".info"
-    in
-    if Filename.check_suffix name suff then
-      Filename.chop_suffix name suff
-    else
-      try
-        Filename.chop_extension name
-      with Invalid_argument _ -> name
-
-let name_out = match !outname with
-| "" -> begin match base_in with
-  | "" -> ""
-  | x  -> begin
-      match !destination with
-      |	Html ->x^".html"
-      |	Text ->x^".txt"
-      |	Info ->x^".info"
+if filter then
+  begin
+    if !fixpoint then
+      Misc.warning ("No fixpoint in filter mode");
+    fixpoint := false
   end
-end    
-| x  -> x
 
 
+let base_out =
+  match !outname with
+  | "" ->
+     begin
+       match base_in with
+       | "" -> ""
+       | _  -> Filename.basename base_in
+     end
+  | name ->
+     let suff =
+       match !destination with
+       | Html -> ".html"
+       | Text -> ".txt"
+       | Info -> ".info"
+     in
+       if Filename.check_suffix name suff then
+         Filename.chop_suffix name suff
+       else
+         try Filename.chop_extension name
+         with Invalid_argument _ -> name
 
-let _ =
-  if !frenchwarning then begin
+
+let name_out =
+  match !outname with
+  | "" ->
+     begin
+       match base_in with
+       | "" -> ""
+       | x  ->
+          begin
+            match !destination with
+            | Html ->x ^ ".html"
+            | Text ->x ^ ".txt"
+            | Info ->x ^ ".info"
+          end
+     end
+  | x -> x
+
+
+let () =
+  if !frenchwarning then
     warning "-francais option is deprecated, use babel instead"
-  end
-
