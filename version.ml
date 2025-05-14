fe9875a02c7f6bd6d9ecdb828cccb9c3b596deb1
Luc Maranget <Luc.Maranget@inria.fr>
2025-05-14 11:07:35 +0200

    [all] Dev version number

--- version.ml
+++ version.ml
@@ -9,8 +9,8 @@
 (*                                                                     *)
 (***********************************************************************)
 
-let real_version = "2.36"
-let release_date = "2022-06-15"
+let real_version = "2.36+1"
+let release_date = "2025-05-14"
 
 let version =
   try
